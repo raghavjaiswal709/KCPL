fa1a3220e41c873127eb2b4b4044990ce653c00c
Raghav jaiswal <125604612+raghavjaiswal709@users.noreply.github.com>
2024-05-04 10:59:15 +0530

    Update Hero.js

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,6 +1,7 @@
 import Right from "@/components/icons/Right";
 import Image from "next/image";
 import "../layout/hero.css"
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -11,13 +12,12 @@ export default function Hero() {
       <p className="subheading text-gray-100">Order Best Quality <span className="spansubtitle"> Fertilizers </span> online for delivery or pickup.</p>
       </div>
         <div className="flex gap-4 mt-16 text-sm">
-          <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
-            Order now
+          <button   className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
+            <Link href="/menu">Order now</Link>
             <Right />
           </button>
-          <button className="flex items-center border-0 gap-2 py-2 text-gray-100 font-semibold">
-            Learn more
-            <Right />
+          <button  className="flex items-center border-0 gap-2 py-2 text-gray-100 font-semibold">
+          <Link href="/menu">Learn more</Link>            <Right />
           </button>
         </div>
       </div>
